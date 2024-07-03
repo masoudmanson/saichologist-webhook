1ec591ed140fcf54ba68958333f694dcfcd5ef8e
Masoud Amjadi <mamjadi@contractor.chanzuckerberg.com>
2024-07-03 01:32:38 -0400

    fix response

--- api/index.ts
+++ api/index.ts
@@ -31,82 +31,23 @@ app.get("/count", async (req, res) => {
     try {
         const result = await sql`SELECT COUNT(id) FROM saichologist;`;
         const count = result.rows[0].count;
-        res.status(200).send(count);
+        res.status(200).send(`
+            <html>
+                <head>
+                    <title>Subscribers</title>
+                    <style>
+                        p {
+                            font-family: mukta, sans-serif;
+                        }
+                    </style>
+                </head>
+                <body>
+                    <p>${count} others has joined</p>
+                </body>
+            </html>
+        `);
     } catch (error) {
         res.status(500).send('Error fetching data!');
-    }
-});
-
-app.get('/allUsers', async (req, res) => {
-    try {
-        const users = await sql`SELECT * FROM Users;`;
-        if (users && users.rows.length > 0) {
-            let tableContent = users.rows
-                .map(
-                    (user) =>
-                        `<tr>
-                        <td>${user.id}</td>
-                        <td>${user.name}</td>
-                        <td>${user.email}</td>
-                    </tr>`
-                )
-                .join('');
-
-            res.status(200).send(`
-                <html>
-                    <head>
-                        <title>Users</title>
-                        <style>
-                            body {
-                                font-family: Arial, sans-serif;
-                            }
-                            table {
-                                width: 100%;
-                                border-collapse: collapse;
-                                margin-bottom: 15px;
-                            }
-                            th, td {
-                                border: 1px solid #ddd;
-                                padding: 8px;
-                                text-align: left;
-                            }
-                            th {
-                                background-color: #f2f2f2;
-                            }
-                            a {
-                                text-decoration: none;
-                                color: #0a16f7;
-                                margin: 15px;
-                            }
-                        </style>
-                    </head>
-                    <body>
-                        <h1>Users</h1>
-                        <table>
-                            <thead>
-                                <tr>
-                                    <th>User ID</th>
-                                    <th>Name</th>
-                                    <th>Email</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                ${tableContent}
-                            </tbody>
-                        </table>
-                        <div>
-                            <a href="/">Home</a>
-                            <a href="/uploadUser">Add User</a>
-                        </div>
-                    </body>
-                </html>
-            `);
-        } else {
-            res.status(404).send('Users not found');
-        }
-    } catch (error) {
-        console.error(error);
-        res.status(500).send('Error retrieving users');
     }
 });
 
